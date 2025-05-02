819568927fe39a0e39bd88cbd2de00b9fbadb904
Akash <ak.parmar1442000@gmail.com>
2025-05-02 10:55:48 +0530

    feat: setting up tailwind config for better control

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ export default {
       colors:{
         primary: '#F66435',
         secondary: '#29292B',
-        white: '#C9C7BA'
+        white: '#C9C7BA',
+        black: '#29292B'
+      },
+      screens: {
+        'xs': '400px',     // Extra small phones
+        'sm': '640px',     // Small devices (default Tailwind)
+        'md': '768px',     // Medium devices (default)
+        'lg': '1024px',    // Large devices (default)
+        'xl': '1280px',    // Extra large (default)
+        '2xl': '1536px',   // 2X large (default)
+        '3xl': '1800px',   // Custom ultra-wide
       }
     },
   },
