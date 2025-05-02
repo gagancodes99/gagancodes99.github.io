f00ef5173bd5a4c5c40ceb92ccfa189058ace9da
Akash <ak.parmar1442000@gmail.com>
2025-05-02 16:18:18 +0530

    ui of bottom bar done

---
+++ src/components/svgs/ArrowIcon.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const ArrowIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+    fill="none"
+    viewBox="0 0 44 26"
+  >
+    <path
+      fill="#C9C7BA"
+      d="M23.848 0v6.319l10.076 6.318-10.076 6.319v6.319L44 12.637z"
+    ></path>
+    <path fill="#C9C7BA" d="M36.171 9.568H0v6.14h36.171z"></path>
+  </svg>
+);
+
+export default ArrowIcon;
---
+++ src/components/svgs/ChevronIcon.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const ChevronIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+    fill="none"
+    viewBox="0 0 22 24"
+  >
+    <path fill="#F66435" d="M5.007 0H0l8.992 12L0 24h5.007L14 12z"></path>
+    <path fill="#F66435" d="M13.007 0H8l8.992 12L8 24h5.007L22 12z"></path>
+  </svg>
+);
+
+export default ChevronIcon;
