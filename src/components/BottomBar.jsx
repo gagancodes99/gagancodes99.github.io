f474f302b832b45164e5f05bd0e02c20ac168d44
Akash <ak.parmar1442000@gmail.com>
2025-05-08 17:40:06 +0530

    added space in the center of the bottombar

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -15,14 +15,17 @@ const BottomBar = () => {
   }
   return (
     <nav className='fixed bottom-0 bg-black h-[64px] lg:h-[72px] w-full grid grid-cols-12 ff-aalto uppercase text-3xl xl:text-4xl tracking-wider '>
-        <div className='hidden sm:grid place-content-center sm:col-span-6 opacity-90  grid-border !border-r-0 !border-b-0 px-2'>
+        <div className='hidden sm:grid place-content-center sm:col-span-5 lg:col-span-3 opacity-90  grid-border !border-r-0 !border-b-0 px-2'>
           <Timer />
         </div> 
+        <div className='hidden sm:grid sm:col-span-3 lg:col-span-5 grid-border !border-r-0 !border-b-0'>
+
+        </div>
         {bottomNav.map((route) => (
         <Link
           key={route}
           to={route}
-          className='h-full col-span-6 sm:col-span-3 grid-border !border-b-0 !border-r-0 flex gap-6 md:gap-4 xl:gap-8 items-center justify-center opacity-90 px-2'
+          className='h-full col-span-6 sm:col-span-2 grid-border !border-b-0 !border-r-0 flex gap-6 md:gap-4 xl:gap-8 items-center justify-center opacity-90 px-2'
         >
           {route === '/' ? 'Home' : route.slice(1).charAt(0).toUpperCase() + route.slice(2)}
           <ArrowIcon className='h-4 md:h-5 xl:h-6 w-auto fill-white' />
