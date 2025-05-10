aba63a729f0c6e95ab46b979d22f6b70fe455588
Akash <ak.parmar1442000@gmail.com>
2025-05-10 23:07:48 +0530

    homepage 3D model functionliaty done

--- src/components/HomeDesk.jsx
+++ src/components/HomeDesk.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BottomBar from './BottomBar'
 import ChevronIcon from './svgs/ChevronIcon'
+import { Canvas } from '@react-three/fiber'
+import HomeScene from './HomeScene'
 
 const HomeDesk = () => {
   return (
-    <section className='h-full grid grid-cols-12'>
+    <section className='h-full w-full grid grid-cols-12 relative'>
       <article className='col-span-5'>
         <header className='ff-aalto text-white/90 text-5xl xl:text-6xl tracking-[1px] flex flex-col items-start grid-border !border-t-0 !border-x-0  p-6'>
           <span>architecting scalable,</span>
@@ -14,7 +16,7 @@ const HomeDesk = () => {
         </header>
         <article className='p-6'>
           <h1 className='text-4xl xs:text-5xl tacking-[1px] ff-aalto text-white/90  relative pl-4'><ChevronIcon className='absolute h-6 w-6 top-1/2 -translate-y-1/2 -left-3 ' /> Who we are</h1>
-          <ul className='list-disc list-outside pl-2 pt-4 font-light text-white/90 text-lg xl:text-xl flex flex-col gap-2'>
+          <ul className='list-disc list-outside pl-2 pt-4 font-light text-white/90 text-base lg:text-base xl:text-xl flex flex-col gap-2'>
             <li> 5+ years crafting dynamic websites, web apps & mobile apps.</li>
             <li> Experts in scalable, high-performance digital solutions.</li>
             <li> Focused on precision, clean code & future-ready architecture.</li>
@@ -24,9 +26,28 @@ const HomeDesk = () => {
           </ul>
         </article>
       </article>
-      <article className='col-span-7 flex flex-col items-start'>
+      <article className='col-span-7 relative flex flex-col items-start'>
         <aside className='h-full w-full grid-border !border-b-0 !border-t-0 !border-r-0 flex items-center justify-center'>
-          <img src="/hero.png" alt="" className='aspect-square w-[300px] h-[300px]'/>
+          {/* <img src="/hero.png" alt="" className='aspect-square w-[300px] h-[300px]'/> */}
+          <Canvas>
+            <ambientLight intensity={0.3} />
+            <directionalLight
+              color={'#fff'}
+              intensity={1.2}
+              position={[5, 5, 5]}
+              castShadow
+              shadow-mapSize-width={1024}
+              shadow-mapSize-height={1024}
+            />
+            <spotLight
+              intensity={2}
+              position={[0, 5, 5]}
+              angle={0.3}
+              penumbra={1}
+              castShadow
+            />
+            <HomeScene />
+          </Canvas>
         </aside>
       </article>
       <BottomBar />
---
+++ src/components/HomeScene.jsx
@@ -0,0 +1,175 @@
+import { Center, OrbitControls } from '@react-three/drei'
+import React, { useMemo, useRef } from 'react'
+import { Points, PointMaterial } from '@react-three/drei'
+import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
+import TextRing from './WrappedText/TextRing'
+import { useControls } from 'leva'
+const HomeScene = () => {
+//     const { position, rotation } = useControls('Torus Transform', {
+//     position: {
+//       value: { x: 1.5, y: 1.8, z: 0 },
+//       min: -5,
+//       max: 5,
+//       step: 0.001,
+//     },
+//     rotation: {
+//       value: { x: 0, y: 0, z: 0.71 },
+//       min: 0,
+//       max: Math.PI * 2,
+//       step: 0.001,
+//     },
+//   })
+    const particlesRef = useRef();
+    const particles = useMemo(() => {
+    const points = []
+    const radius = 1 // Slightly larger than sphere radius
+
+    for (let i = 0; i < 4000; i++) {
+      const phi = Math.acos(2 * Math.random() - 1)
+      const theta = 2 * Math.PI * Math.random()
+
+      const x = radius * Math.sin(phi) * Math.cos(theta)
+      const y = radius * Math.sin(phi) * Math.sin(theta)
+      const z = radius * Math.cos(phi)
+
+      points.push(x, y, z)
+    }
+
+    return new Float32Array(points)
+  }, [])
+ useFrame(() => {
+    if (particlesRef.current) {
+      particlesRef.current.rotation.y += 0.0002
+      particlesRef.current.rotation.x += 0.0002
+    }
+  })
+  return (
+    <>
+      <OrbitControls />
+      <Center>
+        <group scale={1}>
+          <points ref={particlesRef}>
+            <bufferGeometry>
+              <bufferAttribute
+                attach="attributes-position"
+                count={particles.length / 3}
+                array={particles}
+                itemSize={3}
+              />
+            </bufferGeometry>
+            <pointsMaterial color="fff" size={0.02} />
+            <PointMaterial
+              transparent
+              color="#fff"
+              size={0.012}
+              sizeAttenuation={true}
+              depthWrite={true}
+            />
+          </points>
+          <group>
+            <TextRing rotate={[0, 0, 0]}>React Native</TextRing>
+            <TextRing rotate={[Math.PI / 4, Math.PI / 4, 0]}>Next.js</TextRing>
+            <TextRing rotate={[0, Math.PI / 3, Math.PI / 4]}>Nuxt.js</TextRing>
+            <TextRing rotate={[Math.PI / 3, 0, Math.PI / 4]}>
+              CI/CD Pipeline
+            </TextRing>
+            <TextRing rotate={[Math.PI / 6, Math.PI / 4, Math.PI / 2]}>
+              Backend Dev
+            </TextRing>
+            <TextRing rotate={[Math.PI / 4, Math.PI / 6, Math.PI / 3]}>
+              Frontend Dev
+            </TextRing>
+            <TextRing rotate={[Math.PI / 5, Math.PI / 3, Math.PI / 5]}>
+              Figma
+            </TextRing>
+            <TextRing rotate={[Math.PI / 4, Math.PI / 6, Math.PI / 3]}>
+              AWS
+            </TextRing>
+          </group>
+          <mesh rotation={[0, 0, 0]}>
+            <torusGeometry args={[2, 0.3, 16, 100]} />
+            <meshPhysicalMaterial
+              color="#C9C7BA"
+              roughness={0.6}
+              metalness={0.8}
+              clearcoat={1}
+              reflectivity={1}
+              side={THREE.DoubleSide}
+            />
+          </mesh>
+
+          <group>
+            <mesh rotation={[Math.PI, 0, 0]} position={[0, -1.75, 0]}>
+            <torusGeometry args={[2, 0.3, 16, 100, Math.PI * 0.99]} />
+            <meshPhysicalMaterial
+              color="#C9C7BA"
+              roughness={0.6}
+              metalness={0.8}
+              clearcoat={1}
+              reflectivity={1}
+              side={THREE.DoubleSide}
+            />
+          </mesh>
+
+            <mesh rotation={[Math.PI / 2, 0, 0]} position={[2, -1.75, 0]}>
+                <circleGeometry args={[0.3, 32]} />
+                <meshPhysicalMaterial
+                color="#C9C7BA"
+                side={THREE.DoubleSide}
+                roughness={0.6}
+                metalness={0.8}
+                clearcoat={1}
+                reflectivity={1}
+                />
+            </mesh>
+
+            <mesh rotation={[Math.PI / 2, 0, 0]} position={[-2, -1.81, 0]}>
+                <circleGeometry args={[0.3, 32]} />
+                <meshPhysicalMaterial
+                color="#C9C7BA"
+                side={THREE.DoubleSide}
+                roughness={0.6}
+                metalness={0.8}
+                clearcoat={1}
+                reflectivity={1}
+                />
+            </mesh>
+          </group>
+          <mesh
+            position={[1.5, 1.8, 0.0]}
+            rotation={[0, 0, 0.71]}
+            >
+            <torusGeometry args={[0.3, 0.05, 16, 100, Math.PI * 0.7]} />
+            <meshPhysicalMaterial
+                color="#F66435"
+                roughness={0.6}
+                metalness={0.1}
+                clearcoat={1}
+                reflectivity={1}
+                side={THREE.DoubleSide}
+                />
+        </mesh>
+        <mesh
+        position={[
+            1.78,
+            1.94,
+            0,
+        ]}
+        >
+        <sphereGeometry args={[0.1, 32, 32]} />
+        <meshPhysicalMaterial
+            color="#F66435"
+            roughness={0.5}
+            metalness={0.3}
+            clearcoat={1}
+            reflectivity={1}
+        />
+        </mesh>
+        </group>
+      </Center>
+    </>
+  );
+}
+
+export default HomeScene
