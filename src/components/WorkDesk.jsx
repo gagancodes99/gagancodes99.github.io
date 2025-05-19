a2a3c09b56d3d35c35e5a52606e10fdf490e2a1b
Akash <ak.parmar1442000@gmail.com>
2025-05-19 13:49:30 +0530

    feat: layout for project is changed

--- src/components/WorkDesk.jsx
+++ src/components/WorkDesk.jsx
@@ -7,32 +7,46 @@ import { FloatingCard } from './FloatingCard'
 import { Link } from 'react-router-dom'
 const WorkDesk = ({projects, handleCurrProject, currProject}) => {
   return (
-    <section className='h-full grid grid-cols-12 overflow-hidden'>
-    <article className='col-span-5'>
-      <aside className=' grid-border !border-t-0 !border-x-0 p-6'>
-        <header className='ff-aalto text-white/90 text-5xl xl:text-6xl tracking-[1px] flex flex-col items-start uppercase'>
+    <section className='h-full grid grid-cols-12'>
+    <article className='col-span-5 relative h-full'>
+      <aside className='p-6'>
+        <header className='ff-aalto text-white/90 text-4xl xl:text-5xl tracking-[1px] flex flex-col items-start uppercase'>
           <span>{projects[currProject]?.title}</span>
         </header>
-        <p className='text-white/90 text-base lg:text-lg xl:text-xl font-light mt-4'>
+        <p className='text-white/90 text-base xl:text-lg font-light mt-4'>
           {projects[currProject]?.smallDescription}
         </p>
 
       </aside>
-      <article className='px-6 pt-6 pb-0'>
-        <h1 className='text-4xl xs:text-5xl tacking-[1px] ff-aalto relative pl-4'><ChevronIcon className='absolute h-6 w-6 top-1/2 -translate-y-1/2 -left-3' /> TOOLS USED</h1>
-        <ul className='list-disc list-outside pl-2 pt-4 font-light text-white/90 text-base xl:text-xl'>
-           <FloatingCard  projects={projects[currProject]?.tags}/>
+      <article className='pb-0'>
+        <h1 className='text-3xl xs:text-4xl tacking-[1px] ff-aalto relative pl-6 tracking-wide'> TOOLS USED</h1>
+        <ul className='list-outside pt-2 font-light text-white/90 text-base xl:text-xl'>
+           {/* <FloatingCard  projects={projects[currProject]?.tags}/> */}
+           {projects[currProject]?.tags.map((tag, idx)=>(
+              <li key={idx} className='h-12 mt-2 w-full bg-black hover:bg-primary text-primary hover:text-white duration-150 transition-all ff-aalto text-[32px] flex items-center px-8 tracking-wide'>
+                {tag}
+              </li>
+           ))}
         </ul>
       </article>
+
+      <article className='absolute w-full h-16 grid grid-cols-12 grid-border !border-x-0 !border-b-0 bottom-0 bg-black'>
+          <button onClick={()=>handleCurrProject(currProject-1)} className='col-span-4 grid place-content-center grid-border !border-y-0 !border-l-0'>
+            <ArrowIcon className=' w-6 fill-white rotate-180 ' />
+          </button>
+
+          <button onClick={()=>handleCurrProject(currProject+1)} className='col-span-4 grid place-content-center grid-border !border-y-0 text-4xl ff-aalto tracking-wide'>
+            Know More
+          </button>
+          <button onClick={()=>handleCurrProject(currProject+1)} className='col-span-4 grid place-content-center grid-border !border-y-0 !border-r-0'>
+          <ArrowIcon className='w-6 fill-white' />
+        </button>
+      </article>
     </article>
+
+
     <article className='col-span-7 flex flex-col items-start'>
       <aside className='h-full w-full grid-border !border-b-0 !border-t-0 !border-r-0 flex items-center justify-center relative'>
-        <button onClick={()=>handleCurrProject(currProject-1)} className='absolute backdrop-blur-md h-20 w-20 top-1/2 -translate-y-1/2 z-30 left-10 grid place-content-center'>
-          <ArrowIcon className='h-12 w-12 fill-black rotate-180' />
-        </button>
-        <button onClick={()=>handleCurrProject(currProject+1)} className='absolute backdrop-blur-md h-20 w-20 top-1/2 -translate-y-1/2 z-30 right-10 grid place-content-center'>
-          <ArrowIcon className='h-12 w-12 fill-black ' />
-        </button>
         <GridDistortion
           imageSrc={projects[currProject]?.imageUrl}
           grid={10}
